1f743d21047d4afbdcdb4ec36879a1beaa611136
uriel <urrielv@gmail.com>
2021-10-05 09:51:02 -0500

    commit

---
+++ Java/js.js
@@ -0,0 +1,52 @@
+const a_btn = document.getElementById("a")
+const s_btn = document.getElementById("s")
+const d_btn = document.getElementById("d")
+const f_btn = document.getElementById("f")
+const g_btn = document.getElementById("g")
+const h_btn = document.getElementById("h")
+const j_btn = document.getElementById("j")
+const k_btn = document.getElementById("k")
+const l_btn = document.getElementById("l")
+const aa = document.getElementById("A")
+const ss = document.getElementById("S")
+const dd = document.getElementById("D")
+const ff = document.getElementById("F")
+const gg = document.getElementById("G")
+const hh = document.getElementById("H")
+const jj = document.getElementById("J")
+const kk = document.getElementById("K")
+const ll = document.getElementById("L")
+
+a_btn.addEventListener('espacio',()=>{
+    aa.play();
+})
+
+s_btn.addEventListener('click',()=>{
+    ss.play();
+})
+
+d_btn.addEventListener('click',()=>{
+    dd.play();
+})
+f_btn.addEventListener('click',()=>{
+    ff.play();
+})
+
+g_btn.addEventListener('click',()=>{
+    gg.play();
+})
+
+h_btn.addEventListener('click',()=>{
+    hh.play();
+})
+j_btn.addEventListener('click',()=>{
+    jj.play();
+})
+
+k_btn.addEventListener('click',()=>{
+    kk.play();
+})
+
+l_btn.addEventListener('click',()=>{
+    ll.play();
+})
